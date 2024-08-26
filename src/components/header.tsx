7865bcb34e06b37ad4998cf48810e8607e5719c9
Ghaith Thamer <ghaith.th@gmail.com>
2024-08-27 01:36:31 +0300

    renamed HHome to Home

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
     return (
         <div className="text-white w-full absolute z-10">
             <nav className="container relative flex flex-wrap items-center justify-between mx-auto p-8">
-                <Link href="/" className="font-bold text-3xl">HHome</Link>
+                <Link href="/" className="font-bold text-3xl">Home</Link>
                 <div className="space-x-4 text-xl">
                     <Link href="/performance">PPerformance</Link>
                     <Link href="/reliability">RReliability</Link>
